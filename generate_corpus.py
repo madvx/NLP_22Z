4c2403202535acfb313183975b589f419b439b18
jfirlej <jakubfirlej64@gmail.com>
2023-01-08 03:56:39 +0100

    update corpus add confusion matrix and classification report works good!

--- generate_corpus.py
+++ generate_corpus.py
@@ -5,9 +5,10 @@ positive_reviews = []
 neutral_reviews = []
 negative_reviews = []
 
+BALANCED = True
 
 # skip first line i.e. read header first and then iterate over each row od csv as a list
-with open("filtered_data/train_balanced.csv", 'r', encoding='utf-8') as read_obj:
+with open(f"filtered_data/train{'_balanced' if BALANCED else ''}.csv", 'r', encoding='utf-8') as read_obj:
     csv_reader = reader(read_obj)
     header = next(csv_reader)
     # Check file as empty
@@ -23,11 +24,11 @@ with open("filtered_data/train_balanced.csv", 'r', encoding='utf-8') as read_obj
             elif star_rating == "2":
                 negative_reviews.append(row[0])
 
-with open('corpora_nlp22z/positive_reviews_balanced.txt', 'w', encoding='utf-8') as f:
+with open(f"corpora_nlp22z/positive_reviews{'_balanced' if BALANCED else ''}.txt", 'w', encoding='utf-8') as f:
     f.write('\n'.join(positive_reviews))
 
-with open('corpora_nlp22z/neutral_reviews_balanced.txt', 'w', encoding='utf-8') as f:
+with open(f"corpora_nlp22z/neutral_reviews{'_balanced' if BALANCED else ''}.txt", 'w', encoding='utf-8') as f:
     f.write('\n'.join(neutral_reviews))
 
-with open('corpora_nlp22z/negative_reviews_balanced.txt', 'w', encoding='utf-8') as f:
+with open(f"corpora_nlp22z/negative_reviews{'_balanced' if BALANCED else ''}.txt", 'w', encoding='utf-8') as f:
     f.write('\n'.join(negative_reviews))
